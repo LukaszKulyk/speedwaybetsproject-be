7a0ce8652ef15949b697c9afe8d19409d0be53eb
lukasz kulyk <lukasz.kulyk@gmail.com>
2021-10-01 16:30:53 +0200

    Added history mode configuration to the server file.

--- server.js
+++ server.js
@@ -12,8 +12,25 @@ apps.get('/', (req, res) => {
     res.sendFile('src/index.html');
   });
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
-const server = http.createServer(app);
+//const server = http.createServer(app);
 
-server.listen(port);
+//server.listen(port);
+
+
+const server = http.createServer((req, res) => {
+  fs.readFile('index.html', 'utf-8', (err, content) => {
+    if (err) {
+      console.log('We cannot open "index.html" file.')
+    }
+
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    })
+
+    res.end(content)
+  })
+}).listen(port, () => {
+  console.log('Server listening on: http://localhost:%s', port)
+})
